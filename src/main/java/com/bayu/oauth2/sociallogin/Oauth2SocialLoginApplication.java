161ed0039c82a426fecf00999d18d7eb8586c87c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 20:20:02 +0700

    Initial commit

---
+++ src/main/java/com/bayu/oauth2/sociallogin/Oauth2SocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.oauth2.sociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2SocialLoginApplication.class, args);
+	}
+
+}
