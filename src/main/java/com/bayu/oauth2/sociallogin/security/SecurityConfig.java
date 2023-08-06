96e17400a61d4e10524c3da70779f6e3c680f4bd
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-06 19:58:43 +0700

    set csrf, httpRequest and oauth2Login

--- src/main/java/com/bayu/oauth2/sociallogin/security/SecurityConfig.java
+++ src/main/java/com/bayu/oauth2/sociallogin/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.bayu.oauth2.sociallogin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +14,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+        ;
         return http.build();
     }
 
