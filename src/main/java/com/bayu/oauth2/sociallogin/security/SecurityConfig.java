cadf62fb58afcf8bcd60a24fc80e80152300db8c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-06 19:55:51 +0700

    add SecurityConfig

---
+++ src/main/java/com/bayu/oauth2/sociallogin/security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.bayu.oauth2.sociallogin.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
