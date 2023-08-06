02dda3ce532eaaeaf3e91bb3e8abc0de5eb94960
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-06 19:56:52 +0700

    add method securityFilterChain

--- src/main/java/com/bayu/oauth2/sociallogin/security/SecurityConfig.java
+++ src/main/java/com/bayu/oauth2/sociallogin/security/SecurityConfig.java
@@ -1,9 +1,18 @@
 package com.bayu.oauth2.sociallogin.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
 }
